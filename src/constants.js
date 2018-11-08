400f7c9b9b64d303c9e8f0ddbd72d308aa398abc
Tristan Bendixen <angelod1981@gmail.com>
2018-11-09 00:34:55 +0100

    Implemented state handling and character selection.

--- src/constants.js
+++ src/constants.js
@@ -3,17 +3,17 @@ const LEFT = 3;
 const RIGHT = 4;
 const UP = 1;
 
-const STATE_ACTIVE = 3;
 const STATE_CHARSELECT = 4;
 const STATE_ERROR = 2;
 const STATE_INIT = 1;
+const STATE_MAIN = 3;
 
 const Directions = {
   DOWN, LEFT, RIGHT, UP,
 };
 
 const States = {
-  STATE_ACTIVE, STATE_CHARSELECT, STATE_ERROR, STATE_INIT,
+  STATE_MAIN, STATE_CHARSELECT, STATE_ERROR, STATE_INIT,
 };
 
 export { Directions, States };
